224a938398338afc83d30be5cdd9c2031c4c965c
jacob <email@>
2025-09-15 23:27:59 -0500

    Add banner welcome message at the start of the program

--- camelcase.py
+++ camelcase.py
@@ -1,17 +1,20 @@
 def camelcase(sentence):
-    """ Convert sentence to camelCase, for example, "Display all books"
-    is converted to "displayAllBooks" """
-    title_case = sentence.title()  # Uppercase first letter of each word
+    """Convert sentence to camelCase, e.g., 'Display all books' -> 'displayAllBooks'"""
+    title_case = sentence.title()  # Capitalize each word
     upper_camel_cased = title_case.replace(' ', '')  # Remove spaces
-    # Lowercase first letter, join with rest of string
-    # Slices don't produce index out of bounds errors.
-    # So this still works on empty strings or strings of length 1
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    """ Display program name """
+    message = 'Awesome camelcase program!!'
+    stars = '*' * len(message)
+    print(f'\n{stars}\n{message}\n{stars}\n')
+
 def main():
+    banner()
     sentence = input('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
